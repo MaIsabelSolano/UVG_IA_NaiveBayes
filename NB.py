d4e025c1a5a715d75ea740d7b844136fa29de6d2
Isabel Solano <misolanob@gmail.com>
2023-02-11 01:46:48 -0600

    init

---
+++ NB.py
@@ -0,0 +1,14 @@
+#Clase Naive Bayes
+
+# Librerías
+import pandas as pd
+
+class NV(): 
+    def __init__(self, data_entrenamiento):
+        
+        # Lectura de los datos
+
+        # Creación del data frame
+
+        # Limpieza de los datos 
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+# Universidad del Valle de Guatemala
+# Facultad de Ingeniería
+# Departamento de Ciencias de la Computación
+# Inteligencia Artificial
 
+# Christopher García
+# Gabriel Vicente
+# Ma. Isabel Solano 
+
+
+# Obtención de los datos a partir del
